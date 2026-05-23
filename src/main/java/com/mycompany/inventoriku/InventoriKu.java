cda72c885a5dc34cb872b8c87e8356e1353bb872
Althaf-Budiman <budimanaltaf687@gmail.com>
2026-05-23 14:54:20 +0700

    init commit

---
+++ src/main/java/com/mycompany/inventoriku/InventoriKu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventoriku;
+
+/**
+ *
+ * @author HP
+ */
+public class InventoriKu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
